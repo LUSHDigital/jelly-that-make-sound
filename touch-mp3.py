1a54157a35d718facf8b03d407f829a5087f8f65
Simon-Ince <simon.ince@lush.co.uk>
2019-02-19 13:43:59 +0000

    set mode switching

--- touch-mp3.py
+++ touch-mp3.py
@@ -9,7 +9,7 @@ from flask import Flask
 from flask import render_template
 from threading import Thread
 
-sounds = 0
+mode = "sounds"
 
 def flaskThread():
 
@@ -21,14 +21,14 @@ def flaskThread():
 
     @app.route('/sounds')
     def sounds():
-        global sounds
-        sounds = [Sound(path) for path in glob("tracks/.wavs/*.wav")]
+        global mode
+        mode= "sounds"
         return "nothing"
 
     @app.route('/birthday')
     def birthday():
-        global sounds
-        sounds = [Sound(path) for path in glob("birthday-tracks/.wavs/*.wav")]
+        global mode
+        smode = "birthday"
         return "nothing"
 
     app.run(host='0.0.0.0', port= 80)
@@ -52,11 +52,31 @@ def touchThread():
     pygame.mixer.pre_init(frequency=44100, channels=64, buffer=1024)
     pygame.init()
 
-    global sounds
-
-    sounds = [Sound(path) for path in glob("tracks/.wavs/*.wav")]
 
     def play_sounds_when_touched():
+        sounds = [Sound(path) for path in glob("tracks/.wavs/*.wav")]
+        if sensor.touch_status_changed():
+            sensor.update_touch_data()
+
+            is_any_touch_registered = False
+
+            for i in range(num_electrodes):
+                if sensor.get_touch_data(i):
+                    # check if touch is registered to set the led status
+                    is_any_touch_registered = True
+                if sensor.is_new_touch(i):
+                    # play sound associated with that touch
+                    print ("playing sound: " + str(i))
+                    sound = sounds[i]
+                    sound.play()
+
+            if is_any_touch_registered:
+                led.red = 1
+            else:
+                led.off()
+
+    def play_birthday_when_touched():
+        sounds = [Sound(path) for path in glob("birthday-tracks/.wavs/*.wav")]
         if sensor.touch_status_changed():
             sensor.update_touch_data()
 
@@ -80,7 +100,14 @@ def touchThread():
     running = True
     while running:
         try:
-            play_sounds_when_touched()
+            global mode
+            if mode = "sounds":
+                play_sounds_when_touched()
+            elif mode ="birthday"
+                play_birthday_when_touched()
+            else:
+                print("no mode set")
+
         except KeyboardInterrupt:
             led.off()
             running = False
