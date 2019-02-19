842acf90fc24233f693837b48a66e3e4ff677338
Simon-Ince <simon.ince@lush.co.uk>
2019-02-19 10:37:56 +0000

    removed bad comment syntax

--- touch-mp3.py
+++ touch-mp3.py
@@ -45,12 +45,10 @@ from flask import render_template
 from threading import Thread
 
 def flask1():
-    //rendering the HTML page which has the button
     @app.route('/json')
     def json():
         return render_template('json.html')
 
-        //background process happening without any refreshing
         @app.route('/background_process_test')
         def background_process_test():
             print "Hello"
