9f10a1e526b5ca0072eadda4ea40dfe4d7183767
Simon-Ince <simon.ince@lush.co.uk>
2019-02-19 10:24:23 +0000

    added a thread each for flask and touch

--- touch-mp3.py
+++ touch-mp3.py
@@ -41,60 +41,67 @@ from pygame.mixer import Sound
 from glob import glob
 from time import sleep
 from flask import Flask
+from threading import Thread
 
-app = Flask(__name__)
+def flask1():
+    sensor = MPR121.begin()
+    sensor.set_touch_threshold(40)
+    sensor.set_release_threshold(20)
 
-@app.route('/hello')
-def helloIndex():
-    return 'Hello World from Python Flask!'
+    led = RGBLED(6, 5, 26, active_high=False)
 
-app.run(host='0.0.0.0', port= 80)
+    num_electrodes = 12
 
-sensor = MPR121.begin()
-sensor.set_touch_threshold(40)
-sensor.set_release_threshold(20)
+    # convert mp3s to wavs with picap-samples-to-wav
+    led.blue = 1
+    subprocess.call("picap-samples-to-wav tracks", shell=True)
+    led.off()
 
-led = RGBLED(6, 5, 26, active_high=False)
+    # initialize mixer and pygame
+    pygame.mixer.pre_init(frequency=44100, channels=64, buffer=1024)
+    pygame.init()
 
-num_electrodes = 12
+    sounds = [Sound(path) for path in glob("tracks/.wavs/*.wav")]
 
-# convert mp3s to wavs with picap-samples-to-wav
-led.blue = 1
-subprocess.call("picap-samples-to-wav tracks", shell=True)
-led.off()
+    def play_sounds_when_touched():
+        if sensor.touch_status_changed():
+            sensor.update_touch_data()
 
-# initialize mixer and pygame
-pygame.mixer.pre_init(frequency=44100, channels=64, buffer=1024)
-pygame.init()
+            is_any_touch_registered = False
 
-sounds = [Sound(path) for path in glob("tracks/.wavs/*.wav")]
+            for i in range(num_electrodes):
+                if sensor.get_touch_data(i):
+                    # check if touch is registered to set the led status
+                    is_any_touch_registered = True
+                if sensor.is_new_touch(i):
+                    # play sound associated with that touch
+                    print ("playing sound: " + str(i))
+                    sound = sounds[i]
+                    sound.play()
 
-def play_sounds_when_touched():
-    if sensor.touch_status_changed():
-        sensor.update_touch_data()
+            if is_any_touch_registered:
+                led.red = 1
+            else:
+                led.off()
 
-        is_any_touch_registered = False
-
-        for i in range(num_electrodes):
-            if sensor.get_touch_data(i):
-                # check if touch is registered to set the led status
-                is_any_touch_registered = True
-            if sensor.is_new_touch(i):
-                # play sound associated with that touch
-                print ("playing sound: " + str(i))
-                sound = sounds[i]
-                sound.play()
-
-        if is_any_touch_registered:
-            led.red = 1
-        else:
+    running = True
+    while running:
+        try:
+            play_sounds_when_touched()
+        except KeyboardInterrupt:
             led.off()
+            running = False
+        sleep(0.01)
 
-running = True
-while running:
-    try:
-        play_sounds_when_touched()
-    except KeyboardInterrupt:
-        led.off()
-        running = False
-    sleep(0.01)
+def touch2():
+    app = Flask(__name__)
+
+    @app.route('/hello')
+    def helloIndex():
+        return 'Hello World from Python Flask!'
+
+    app.run(host='0.0.0.0', port= 80)
+
+if __name__ == '__main__':
+    Thread(target = flask1).start()
+    Thread(target = touch2).start()
